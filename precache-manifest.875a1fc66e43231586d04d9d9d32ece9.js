f215168f1ae513db990e20bc7dc36e71fd92205a
dev <tokenkitadev@gmail.com>
2020-01-24 14:10:17 +0700

    first deploy

---
+++ precache-manifest.875a1fc66e43231586d04d9d9d32ece9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5302b7f816b610ee91e7f17e2bcda79d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b620551261b1dcfbcd",
+    "url": "/static/css/main.85a49355.chunk.css"
+  },
+  {
+    "revision": "9688fc5a0818b7e6162c",
+    "url": "/static/js/2.1856fce3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.1856fce3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6b620551261b1dcfbcd",
+    "url": "/static/js/main.c043f883.chunk.js"
+  },
+  {
+    "revision": "632aa3fa6b0f264fad2a",
+    "url": "/static/js/runtime-main.5c0222a8.js"
+  }
+]);
